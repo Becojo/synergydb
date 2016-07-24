4c389198211280f96b8544dce850883da14a5f4e
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2016-07-24 12:00:35 -0400

    fixed tests

--- spec/synergydb_spec.rb
+++ spec/synergydb_spec.rb
@@ -11,31 +11,31 @@ describe Synergydb do
     db = Synergydb::Database.new
 
     commands = [
-      [["create", "count", "Max[Int]", 3], :ok],
-      [["set", "count", 30], :ok],
-      [["set", "count", 2], :ok],
-      [["get", "count"], 30],
+      [["create", "count", "Max[Int]", 3], [:ok, "count"]],
+      [["set", "count", 30], [:ok]],
+      [["set", "count", 2], [:ok]],
+      [["get", "count"], [:ok, 30]],
 
-      [["ping"], :pong],
+      [["ping"], [:ok, :pong]],
 
-      [["create", "votes", "Map[Min[Timestamped[Str]]]"], :ok],
-      [["set", "votes", "user1", [0, "candidate1"]], :ok],
-      [["set", "votes", "user1", [10, "candidate2"]], :ok],
-      [["get", "votes", "user1"], [0, "candidate1"]],
+      [["create", "votes", "Map[Min[Timestamped[Str]]]"], [:ok, "votes"]],
+      [["set", "votes", "user1", [0, "candidate1"]], [:ok]],
+      [["set", "votes", "user1", [10, "candidate2"]], [:ok]],
+      [["get", "votes", "user1"], [:ok, [0, "candidate1"]]],
 
-      [["create", "something", "Map[Max[Int]]"], :ok],
-      [["set", "something", "key1", 4], :ok],
-      [["get", "something", "key1"], 4],
-      [["set", "something", "key1", 10], :ok],
-      [["get", "something", "key1"], 10],
+      [["create", "something", "Map[Max[Int]]"], [:ok, "something"]],
+      [["set", "something", "key1", 4], [:ok]],
+      [["get", "something", "key1"], [:ok, 4]],
+      [["set", "something", "key1", 10], [:ok]],
+      [["get", "something", "key1"], [:ok, 10]],
 
-      [["create", "users", "Map[Max[Timestamped[Map[Str]]]]"], :ok],
-      [["set", "users", "user1", [10,  { name: "bob" }]], :ok],
-      [["get", "users", "user1"], [10, { name: "bob" }]],
-      [["set", "users", "user1", [11,  { name: "bobby" }]], :ok],
-      [["get", "users", "user1"], [11, { name: "bobby"}]],
-      [["set", "users", "user1", [9,   { name: "bob the great" }]], :ok],
-      [["get", "users", "user1"], [11, { name: "bobby"}]],
+      [["create", "users", "Map[Max[Timestamped[Map[Str]]]]"], [:ok, 'users']],
+      [["set", "users", "user1", [10,  { name: "bob" }]], [:ok]],
+      [["get", "users", "user1"], [:ok, [10, { name: "bob" }]]],
+      [["set", "users", "user1", [11,  { name: "bobby" }]], [:ok]],
+      [["get", "users", "user1"], [:ok, [11, { name: "bobby"}]]],
+      [["set", "users", "user1", [9,   { name: "bob the great" }]], [:ok]],
+      [["get", "users", "user1"], [:ok, [11, { name: "bobby"}]]],
     ]
 
     commands.each do |(command, expected)|
